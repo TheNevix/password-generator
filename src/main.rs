23a99d80e492e3fa369ff6d411b8eb6baf4df4a2
TheNevix <ducelliersaimen@gmail.com>
2024-04-15 13:07:25 +0200

    Rewrote the full program to generate a pass without using a password generator crate.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io;
-use passwords::PasswordGenerator;
+use std::io::{self, Error};
+use rand::{seq::IteratorRandom, *};
+use rand::seq::SliceRandom;
 
 fn main() {
     //print info message
@@ -8,31 +9,45 @@ fn main() {
     println!("Enter the amount of passwords you want to generate:");
 
     //amount of passwords
-    let amount_of_passwords: u32;
+    let mut amount_of_passwords: u32;
+    let mut amount_of_chars: u32;
 
-    //as long as we have no valid amount
-    loop{
-        //var to store amount of passes input
-        let mut amount_of_passwords_input = String::new();
+    //ask for amount of pass
+    loop {
+        //get the amount
+        let result = ask_input("How many passwords do you want to generate?");
 
-        //get the input
-        io::stdin()
-            .read_line(&mut amount_of_passwords_input)
-            .expect("Must be a num");
-
-        //check
-        match amount_of_passwords_input.trim().parse() {
-            Ok(number) => {
-                amount_of_passwords = number;
+        //check if okay or error
+        match result {
+            Ok(num ) => {
+                amount_of_passwords = num;
                 break;
             },
-            Err(_) => println!("Please enter a valid number")
-        };
-        
+            Err(_) => println!("Please enter a valid number!")
+        }
     }
 
-    //generate passwords
-    let passwords: Vec<String> = generate_passwords(amount_of_passwords);
+    //ask for amount of chars
+    loop {
+        //get the amount
+        let result = ask_input("How many chars should each password contain?");
+
+        //check if okay or error
+        match result {
+            Ok(num ) => {
+                amount_of_chars = num;
+                break;
+            },
+            Err(_) => println!("Please enter a valid number!")
+        }
+    }
+
+    let mut passwords: Vec<String> = Vec::new();
+
+    while (passwords.len() as u32) < amount_of_passwords {
+        let password = generate_password(amount_of_chars);
+        passwords.push(password);
+    }
 
     println!("Here are your {} passwords:", amount_of_passwords);
 
@@ -49,23 +64,90 @@ fn main() {
     io::stdin().read_line(&mut _input).expect("Failed to read input");
 }
 
-fn generate_passwords(amount: u32) -> Vec<String> {
+//ask for user input
+fn ask_input(question: &str) -> Result<u32, Error> {
 
-    //create the pg
-    let pass = PasswordGenerator{
-        length: 16,
-        numbers: true,
-        lowercase_letters: true,
-        uppercase_letters: true,
-        symbols: true,
-        spaces: false,
-        exclude_similar_characters: false,
-        strict: true
+    //print the line of text
+    println!("{}", question);
+
+    //create input var
+    let mut result = String::new();
+
+    //read the input
+    // Read the input
+    io::stdin()
+        .read_line(&mut result)
+        .map(|_| {
+            result.trim().parse::<u32>().map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))
+        })
+        .and_then(|res| res)
+}
+
+fn generate_password(mut char_amount: u32) -> String{
+    //you always want capital letters, normal letters, numbers and special chars
+    let divided_by_four: u32 =  char_amount / 4;
+    let mut password = String::new();
+    let mut rng = rand::thread_rng();
+
+    //generate capital letters
+    {
+        let amount_of_capital_letters: u32 = rng.gen_range(1..=divided_by_four);
+        let capital_letters: String = (0..amount_of_capital_letters)
+        .map(|_| {
+            let letter = (rng.gen_range(b'A'..=b'Z') as char).to_string();
+            letter
+        })
+        .collect();
+        char_amount = char_amount - amount_of_capital_letters;
+        password.push_str(&capital_letters)
     };
 
-    //generate the amount of passwords
-    let passwords_result = pass.generate(amount as usize).unwrap();
+    //generate lowercase letters
+    {
+        let amount_of_capital_letters: u32 = rng.gen_range(1..=divided_by_four);
+        let capital_letters: String = (0..amount_of_capital_letters)
+        .map(|_| {
+            let letter = (rng.gen_range(b'a'..=b'z') as char).to_string();
+            letter
+        })
+        .collect();
+        char_amount = char_amount - amount_of_capital_letters;
+        password.push_str(&capital_letters)
+    }
 
-    //return result
-    passwords_result
+    //generate numbers
+    {
+        let amount_of_capital_letters: u32 = rng.gen_range(1..=divided_by_four);
+        let capital_letters: String = (0..amount_of_capital_letters)
+        .map(|_| {
+            let letter = (rng.gen_range(b'0'..=b'9') as char).to_string();
+            letter
+        })
+        .collect();
+        char_amount = char_amount - amount_of_capital_letters;
+        password.push_str(&capital_letters)
+    }
+
+    //special chars
+    {
+        let special_chars = "!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+        let amount_of_special_chars: u32 = char_amount;
+        let mut special_chars_string = String::new();
+        while (special_chars_string.len() as u32) < amount_of_special_chars {
+            let random_char = special_chars.chars().choose(&mut rand::thread_rng()).unwrap();
+            special_chars_string.push(random_char);
+        }
+        password.push_str(&special_chars_string)
+    }
+
+    let shuffled_password = shuffle_string(&password);
+    shuffled_password
+}
+
+//shuffle generated string
+fn shuffle_string(input: &String) -> String {
+ let mut chars: Vec<char> = input.chars().collect();
+    let mut rng = thread_rng();
+    chars.shuffle(&mut rng);
+    chars.into_iter().collect()
 }
